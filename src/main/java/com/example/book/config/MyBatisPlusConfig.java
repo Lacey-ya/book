3d3d67569ccbfdd20c0ef73c094620d7b0ec61cc
Lacey <1365973751@qq.com>
2020-03-15 16:32:10 +0800

    Revert "Revert "新增mybatis plus配置类""
    This reverts commit 4efc00d

---
+++ src/main/java/com/example/book/config/MyBatisPlusConfig.java
@@ -0,0 +1,20 @@
+package com.example.book.config;
+
+
+import com.baomidou.mybatisplus.plugins.PaginationInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyBatisPlusConfig {
+
+    /**
+     * @Description : mybatis-plus分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        paginationInterceptor.setDialectType("mysql");
+        return paginationInterceptor;
+    }
+}
